97b73ed8a0f5f42417d654ec7b04737db8ea86dc
David Whipple <david@inators.net>
2018-06-26 20:56:54 -0600

    Basic web page with placeholders

---
+++ data.js
@@ -0,0 +1,9 @@
+
+document.getElementById("coldTemp").innerHTML = "1&#176;F";
+document.getElementById("hotTemp").innerHTML = "2&#176;F";
+document.getElementById("coldHum").innerHTML = "3%";
+document.getElementById("hotHum").innerHTML = "4%";
+document.getElementById("headStat").innerHTML = "5";
+document.getElementById("time").innerHTML = "6";
+
+

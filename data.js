86cb073366be4808f7393a5cea15d126b0a02c38
David Whipple <david@inators.net>
2018-06-26 21:52:17 -0600

    a little style and saved on bytes in the js file since this may be embedded

--- data.js
+++ data.js
@@ -1,9 +1,11 @@
 
-document.getElementById("coldTemp").innerHTML = "1&#176;F";
-document.getElementById("hotTemp").innerHTML = "2&#176;F";
-document.getElementById("coldHum").innerHTML = "3%";
-document.getElementById("hotHum").innerHTML = "4%";
-document.getElementById("headStat").innerHTML = "5";
-document.getElementById("time").innerHTML = "6";
+var $ = function( id ) { return document.getElementById( id ); };
+
+$("coldTemp").innerHTML = "1&#176;F";
+$("hotTemp").innerHTML = "2&#176;F";
+$("coldHum").innerHTML = "3%";
+$("hotHum").innerHTML = "4%";
+$("headStat").innerHTML = "5";
+$("time").innerHTML = "7";
 
 
